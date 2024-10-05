e36f25cd38afe127559bff59acf58634f3c8ed9e
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-10-05 21:13:07 +0545

    sds

--- backend.js
+++ backend.js
@@ -1,9 +1,6 @@
-//creating an express server:
+//creating an express server:-
 
-//importing the express package in the express variable. It gets every functionalities that the express module has exported.
-//it is a function that is imported so, express(variable) is now a function.
 const express = require("express");
-
-//creating an instance of the express aplication.
-//as everything is in the express() so we need to create a instance of the function which represents us our express application.
 const app = express();
+
+//creating route 
