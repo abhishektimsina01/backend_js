d526c0acc6db688a04739cff6fa1a42ed8b42b30
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-10-05 22:19:06 +0545

    sdsd

--- backend.js
+++ backend.js
@@ -1,13 +1,17 @@
 //creating an express server:-
 
 const express = require("express");
+const res = require("express/lib/response");
 const app = express();
 
-//creating route 
-app.get("./", (req, res)=>{
-    res.send("This is the main page.");
+app.get("/", (req, res)=>{
+    res.send("This is the Main page");
 })
 
-app.listen(3000, ()=>{
-    console.log("Server has been started.");
+app.get("/about", (req,res)=>{
+    res.send("this is the About page.")
+})
+
+app.listen(3000, (err)=>{
+    console.log("the server has been connected");
 })
