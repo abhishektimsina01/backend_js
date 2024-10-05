9b7e14fe70eeb863863ea705b13abc3b6993bfe8
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-10-05 21:27:32 +0545

    dsadsad

--- backend.js
+++ backend.js
@@ -4,3 +4,10 @@ const express = require("express");
 const app = express();
 
 //creating route 
+app.get("./", (req, res)=>{
+    res.send("This is the main page.");
+})
+
+app.listen(3000, ()=>{
+    console.log("Server has been started.");
+})
