4d6b862a3ec3c5d088b97490e5bd09edc96f2c91
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-10-06 21:02:43 +0545

    dsds

--- backend.js
+++ backend.js
@@ -4,14 +4,19 @@ const express = require("express");
 const res = require("express/lib/response");
 const app = express();
 
+//routing => get/post/patch... http method request to the specified route/path then the app.method() triggers the handler function which handles the function 
 app.get("/", (req, res)=>{
     res.send("This is the Main page");
 })
 
-app.get("/about", (req,res)=>{
+app.post("/", (req,res)=>{
+    res.send("Posted");
+})
+
+app.get("/About", (req,res)=>{
     res.send("this is the About page.")
 })
 
-app.listen(3000, (err)=>{
+app.listen(8000, (err)=>{
     console.log("the server has been connected");
 })
