bbc64feda0bbb6001d1c6475f293f44f7704c0a4
Abhishek_Timsina <timsinaabhishek1@gmail.com>
2024-10-16 18:00:34 +0545

    sdsd

--- backend.js
+++ backend.js
@@ -1,76 +1,30 @@
-//creating an express server:-
+const express = require("express")
+const app = express()
+const fs = require("fs")
+const users_data = require("./MOCK_DATA.json")
 
-const express = require("express");
-const app = express();
 
-app.use((req,res,next)=>{
-    if(req.url == "/" || "/about"){
-        console.log("Running the middleware before going to the route",req.url);
-        console.log(req.headers);        
+app.get("/users",(req,res)=>{
+    res.send(`This is the ${req.path}`)
+})
+
+app.get("/users/:id",(req,res)=>{
+    const id = req.params.id
+    console.log(id)
+    fs.appendFile("log.txt", `${JSON.stringify(users_data.find(user=> user.id == id))} \n\n`,(err)=>{
+        if(err){
+            console.log("an error has been encountered")
+            res.status(500).send("error")
+        }
+    } )
+    
+})
+
+app.listen(3000,(err)=>{
+    if(err){
+        console.log("an error has been encountered")
     }
     else{
-        res.send(404);
+        console.log("Server has been connected")
     }
-    next();
-});
-
-app.use("/About",(req,res,next)=>{
-    console.log("time is",Date.now())
-    next()
-},(req,res,next)=>{
-    console.log("hello")
-    next()
-})
-
-app.get("/", function(req, res){
-    res.send("This is the Main page");
-})
-
-app.get("/About", (req,res,next)=>{
-    res.end("this is the About page.");
-    next('route')
-})
-
-app.use((req,res,next)=>{
-    console.log("this is the middleware for the route", req.path)
-    next()
-})
-
-app.get("/About",(req,res)=>{
-    console.log(`this is the second route for the path ${req.path}`)
-})
-
-// "/home" is path
-
-app.use("/home",(res,req,next)=>{
-    console.log("this is the /home path")
-    next()
-},function (req,res,next){
-    console.log("this is the /home page")
-    next()
-})
-
-app.get("/home",(req,res,next)=>{
-    res.end("This is the home page")
-})
-
-
-//router-level middleware
-const router1  = express.Router()
-const router2 = express.Router()
-
-router1.get("/home",(req,res)=>{
-    res.end("this is the home page for the user")
-})
-
-router2.get("/home",(req,res)=>{
-    res.end("this is the admin page for the admin")
-})
-
-app.use("/user",router1)
-app.use("/admin",router2)
-
-//open the server a=on the port 3000 and then listen for th requaest there
-app.listen(3000, (err)=>{
-    console.log("the server has been connected");
 })
